899b73868be925960a0cacc09e27373801c6b491
hoang44444444 <58798071+hoang44444444@users.noreply.github.com>
2022-02-20 11:10:37 +0700

    statistics

---
+++ VanLangDoctor/VanLangDoctor/Areas/Admin/Controllers/QL_ThongKeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VanLangDoctor.Areas.Admin.Controllers
+{
+    public class QL_ThongKeController : Controller
+    {
+        // GET: Admin/QL_ThongKe
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
