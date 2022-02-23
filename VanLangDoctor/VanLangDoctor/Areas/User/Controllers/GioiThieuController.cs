268ae92055a694604100927f6bc6b71980e27ae6
187PM06602 - DANG PHU HUNG - T5 <hungdp2612@gmail.com>
2022-02-23 14:07:23 +0700

    update db

---
+++ VanLangDoctor/VanLangDoctor/Areas/User/Controllers/GioiThieuController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VanLangDoctor.Areas.User.Controllers
+{
+    public class GioiThieuController : Controller
+    {
+        // GET: User/GioiThieu
+        public ActionResult Gioithieu()
+        {
+            return View();
+        }
+    }
+}
