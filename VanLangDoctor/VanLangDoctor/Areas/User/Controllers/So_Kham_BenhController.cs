e36a4d03f67fcf4c703286f151ef54f422fc0cfc
187PM06602 - DANG PHU HUNG - T5 <hungdp2612@gmail.com>
2022-03-10 00:18:57 +0700

    skb_v1

--- VanLangDoctor/VanLangDoctor/Areas/User/Controllers/So_Kham_BenhController.cs
+++ VanLangDoctor/VanLangDoctor/Areas/User/Controllers/So_Kham_BenhController.cs
@@ -14,68 +14,36 @@ namespace VanLangDoctor.Areas.User.Controllers
         CP24Team02Entities db = new CP24Team02Entities();
 
         // GET: User/So_Kham_Benh
-        public ActionResult SoKhamBenh(int Id)
+        public ActionResult SoKhamBenh(int? id)
         {
-            var skb = db.SO_KHAM_BENH.Include(s => s.BENH_NHAN).Include(s => s.ID_SOKHAMBENH).Where(s => s.ID_SOKHAMBENH == Id);
-            return View(skb);
-        }
+            var skb = db.SO_KHAM_BENH.FirstOrDefault(s => s.ID_BENH_NHAN == id);
 
-
-        //GET: User/So_Kham_Benh/Update
-        [HttpGet]
-        public ActionResult Update_SKB(int? Id)
-        {
-            if (Id == null)
-            {
-                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
-            }
-            var skb = db.SO_KHAM_BENH.Find(Id);
             if (skb == null)
             {
-                return HttpNotFound();
+                return RedirectToAction("DangKy_SKB");
             }
             return View(skb);
+
+        }
+        public ActionResult DangKy_SKB()
+        {
+            ViewBag.ID_BENH_NHAN = new SelectList(db.BENH_NHAN, "ID_BENH_NHAN", "TEN_BN");
+            return View();
         }
 
-
-        //POST: User/So_Kham_Benh/Update
         [HttpPost]
-        public ActionResult Update_SKB(SO_KHAM_BENH sO_KHAM_BENH)
+        [ValidateAntiForgeryToken]
+        //POST: User/Dang ky So
+        public ActionResult DangKy_SKB(SO_KHAM_BENH sO_KHAM_BENH)
         {
+            var skb = db.SO_KHAM_BENH.FirstOrDefault(s => s.ID_BENH_NHAN == sO_KHAM_BENH.ID_BENH_NHAN);
             if (ModelState.IsValid)
             {
-                using (var scope = new TransactionScope())
-                {
-                    db.Entry(sO_KHAM_BENH).State = EntityState.Modified;
-                    db.SaveChanges();
-
-                    scope.Complete();
-                    TempData["Success"] = "Cập nhật thành công!!!";
-                    return RedirectToAction("SoKhamBenh");
-                }
+                skb = db.SO_KHAM_BENH.Find(id)
+                db.SO_KHAM_BENH.Add(sO_KHAM_BENH);
+                db.SaveChanges();
+                return RedirectToAction("SoKhamBenh");
             }
-            db.Entry(sO_KHAM_BENH).State = EntityState.Modified;
-            TempData["Success"] = "Cập nhật thành công!!!";
-
-            return View(sO_KHAM_BENH);
-        }
-
-
-        //POST: User/So_Kham_Benh/Create
-        public ActionResult Create_SKB(SO_KHAM_BENH sO_KHAM_BENH)
-        {
-            if (ModelState.IsValid)
-            {
-                using (var scope = new TransactionScope())
-                {
-                    db.SO_KHAM_BENH.Add(sO_KHAM_BENH);
-                    db.SaveChanges();
-
-                    scope.Complete();
-                }
-            }
-            TempData["Success"] = "Đăng ký sổ thành công!!!";
-
             return RedirectToAction("SoKhamBenh");
         }
     }
