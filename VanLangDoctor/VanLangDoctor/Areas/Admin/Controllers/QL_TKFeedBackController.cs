471cb9d189e773d30d04561b042ea174871b837f
187PM06602 - DANG PHU HUNG - T5 <hungdp2612@gmail.com>
2022-04-07 15:20:38 +0700

    chart feedback

---
+++ VanLangDoctor/VanLangDoctor/Areas/Admin/Controllers/QL_TKFeedBackController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using VanLangDoctor.Models;
+
+namespace VanLangDoctor.Areas.Admin.Controllers
+{
+    public class QL_TKFeedBackController : Controller
+    {
+        private CP24Team02Entities db = new CP24Team02Entities();
+
+        // GET: Admin/QL_TKFeedBack
+        public ActionResult Index(string dg = "")
+        {
+            var danhgia = db.FEEDBACKs.Where(tk => tk.DANH_GIA == dg).ToList();
+            ViewBag.dg = dg;
+            return View(danhgia);
+        }
+        [HttpPost]
+        public JsonResult ChartData(string dg = "")
+        {
+            var noidung = db.FEEDBACKs.Where(item => item.DANH_GIA == dg)
+                .Select(item => new { name = item.DANH_GIA, count = item.DANH_GIA.Count() }).ToList();
+            return Json(new { dbchart = noidung, code = 200 }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
