c610d70af3d3b2a214dd4a6d7d7662ea2b58d6cd
hotanminhtam <hotanminhtam1703@gmail.com>
2022-04-18 17:14:14 +0700

    Update View News

--- VanLangDoctor/VanLangDoctor/Areas/User/Controllers/TinTucController.cs
+++ VanLangDoctor/VanLangDoctor/Areas/User/Controllers/TinTucController.cs
@@ -17,8 +17,11 @@ namespace VanLangDoctor.Areas.User.Controllers
         // GET: User/TinTuc
         public ActionResult Tin_Tuc()
         {
-            ViewBag.Top = GetTopViews();
-            return View(db.TIN_TUC.ToList());
+            ViewBag.Top1 = GetTop1View();
+            ViewBag.Top3 = GetTop3Views();
+            ViewBag.RecentArticles = GetRecentArticles();
+            var TinTuc = db.TIN_TUC.ToList();
+            return View(TinTuc);
         }
 
         private const string PICTURE_PATH = "~/Content/IMG_NEWS/";
@@ -43,15 +46,29 @@ namespace VanLangDoctor.Areas.User.Controllers
             }
             tIN_TUC.CountViews += 1;
             db.SaveChanges();
-            ViewBag.Top = GetTopViews();
+            ViewBag.Top = GetTop3Views();
             return View(tIN_TUC);
         }
 
-        //GET: Top Views
-        private List<TIN_TUC> GetTopViews()
+        //GET: TOP 1 View
+        public TIN_TUC GetTop1View()
         {
-            return db.TIN_TUC.OrderByDescending(t => t.CountViews).Take(3).ToList();
+            return db.TIN_TUC.OrderByDescending(item => item.CountViews).First();
         }
+
+
+        //GET: Top Views
+        private List<TIN_TUC> GetTop3Views()
+        {
+            return db.TIN_TUC.OrderByDescending(t => t.CountViews).Skip(1).Take(3).ToList();
+        }
+
+        //GET: Recent Articles
+        public List<TIN_TUC> GetRecentArticles()
+        {
+            return db.TIN_TUC.OrderByDescending(t=>t.NGAY_DANG).Take(5).ToList();
+        }
+
 
         protected override void Dispose(bool disposing)
         {
