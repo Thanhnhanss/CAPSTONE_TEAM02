797d32141290364ba3192fb9c991f8e4fcdf461a
187PM06602 - DANG PHU HUNG - T5 <hungdp2612@gmail.com>
2022-04-18 13:43:17 +0700

    user login in mobile

--- VanLangDoctor/VanLangDoctor/Areas/Admin/Controllers/HomeAdminController.cs
+++ VanLangDoctor/VanLangDoctor/Areas/Admin/Controllers/HomeAdminController.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -13,6 +16,20 @@ namespace VanLangDoctor.Areas.Admin.Controllers
         public ActionResult HomeAdmin()
         {
             return View();
+        }
+        [HttpPost]
+        public async Task<JsonResult> ChartData()
+        {
+            var keys = new string[] { "Rất không hài lòng", "Chưa hài lòng", "Bình thường", "Hài lòng", "Rất hài lòng" };
+            var httpClient = new HttpClient();
+            var chartData = await httpClient.GetStringAsync("https://thongkevludoctor.herokuapp.com/api/statistic");
+            var data = JsonConvert.DeserializeObject<Dictionary<int, int>>(chartData).ToDictionary(e => keys[e.Key - 1], e => e.Value);
+            foreach (var key in keys)
+            {
+                if (!data.ContainsKey(key))
+                    data.Add(key, 0);
+            }
+            return Json(new { dbchart = data.Select(e => new { e.Key, Count = e.Value }), code = 200 }, JsonRequestBehavior.AllowGet);
         }
     }
 }
---
+++ VanLangDoctor/VanLangDoctor/Areas/Admin/Controllers/QL_TKBookCalendarController.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VanLangDoctor.Areas.Admin.Controllers
+{
+    public class QL_TKBookCalendarController : Controller
+    {
+        // GET: Admin/QL_TKBookCalendar
+        public ActionResult BookCalendar()
+        {
+            return View();
+        }
+        // GET: Admin/TKBookCalendar
+        public async Task<JsonResult> ChartData()
+        {
+            var httpClient = new HttpClient();
+            var chartData = await httpClient.GetStringAsync("https://bacsivanlang-statistic.herokuapp.com/api/statistic?startDate=01%2F2022&endDate=12%2F2022");
+            var data = JsonConvert.DeserializeObject<Dictionary<string, int>>(chartData);
+            return Json(new { dbchart = data.Select(e => new { e.Key, Count = e.Value }), code = 200 }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
