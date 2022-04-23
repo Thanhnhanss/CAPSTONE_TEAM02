a1430c0a20fa56cd7887610f3877ddbadd9102cb
187PM06602 - DANG PHU HUNG - T5 <hungdp2612@gmail.com>
2022-04-23 22:28:23 +0700

    statictis medical record

---
+++ VanLangDoctor/VanLangDoctor/Areas/Admin/Controllers/Ql_TKDonThuocController.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using VanLangDoctor.Models;
+
+namespace VanLangDoctor.Areas.Admin.Controllers
+{
+    [Authorize(Roles = "Quản trị viên, Bác sĩ, Quản lý")]
+    public class QL_TKDonThuocController : Controller
+    {
+        private CP24Team02Entities db = new CP24Team02Entities();
+
+        // GET: Admin/HomeAdmin
+        public ActionResult Index(int ? month)
+        {
+            month = month ?? DateTime.Now.Month  ;
+            ViewBag.month = month;
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult ChartData(int? month)
+        {
+            month = month ?? DateTime.Now.Month;
+            if (month < 1 || month > 12)
+                throw new ArgumentException();
+            var data = db.DON_THUOC
+                .Where(donThuoc => donThuoc.NGAY_LAP.Value.Month == month && donThuoc.NGAY_LAP.Value.Year == DateTime.Now.Year)
+                .GroupBy(e => e.BACSI.TEN_BACSI)
+                .Select(e => new { e.Key, Count = e.Count() });
+            return Json(new { dbchart = data }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
