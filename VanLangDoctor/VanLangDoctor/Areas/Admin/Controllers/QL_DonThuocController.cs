7b5d718514ab57a4875abd7dcaf011a5b0484fe7
187PM06602 - DANG PHU HUNG - T5 <hungdp2612@gmail.com>
2022-04-24 11:09:06 +0700

    Chi tiet don thuoc user

--- VanLangDoctor/VanLangDoctor/Areas/Admin/Controllers/QL_DonThuocController.cs
+++ VanLangDoctor/VanLangDoctor/Areas/Admin/Controllers/QL_DonThuocController.cs
@@ -1,127 +1,197 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
 using System.Linq;
 using System.Net;
-using System.Transactions;
 using System.Web;
 using System.Web.Mvc;
 using VanLangDoctor.Models;
 
 namespace VanLangDoctor.Areas.Admin.Controllers
 {
-    [Authorize(Roles = "Quản trị viên, Bác sĩ, Quản lý")]
     public class QL_DonThuocController : Controller
     {
         private CP24Team02Entities db = new CP24Team02Entities();
-        private List<DON_THUOC> DonThuoc = null;
-        public QL_DonThuocController()
-        {
-            var session = System.Web.HttpContext.Current.Session;
-            if (session["DonThuoc"] != null)
-                DonThuoc = session["DonThuoc"] as List<DON_THUOC>;
-            else
-            {
-                DonThuoc = new List<DON_THUOC>();
-                session["DonThuoc"] = DonThuoc;
-            }
-        }
 
+        // GET: Admin/QL_DonThuoc
+        [HttpGet]
         public ActionResult Index()
         {
-            var hashtable = new Hashtable();
-            foreach (var donthuoc in DonThuoc)
+            var dON_THUOC = db.DON_THUOC.Include(d => d.BACSI).Include(d => d.SO_KHAM_BENH);
+            return View(dON_THUOC.ToList());
+        }
+        // GET: Admin/QL_DonThuoc/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
             {
-                if (hashtable[donthuoc.THUOC.ID_THUOC] != null)
-                {
-                    (hashtable[donthuoc.THUOC.ID_THUOC] as DON_THUOC).CHI_DINH += donthuoc.CHI_DINH;
-                }
-                else hashtable[donthuoc.THUOC.ID_THUOC] = donthuoc;
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            DonThuoc.Clear();
-            foreach (DON_THUOC donthuoc in hashtable.Values)
-                DonThuoc.Add(donthuoc);
-            return View(DonThuoc);
-        }
-        [HttpPost]
-        public ActionResult Create(int ID_Thuoc)
-        {
-            var thuoc = db.THUOCs.Find(ID_Thuoc);
-            DonThuoc.Add(new DON_THUOC
+            DON_THUOC dON_THUOC = db.DON_THUOC.Find(id);
+            if (dON_THUOC == null)
             {
-                THUOC = thuoc
-            });
-            ViewBag.ID_BACSI = new SelectList(db.BACSIs, "ID_BACSI", "TEN_BACSI");
-            ViewBag.ID_THUOC = new SelectList(db.THUOCs, "ID_THUOC", "TEN_THUOC");
-            return RedirectToAction("Index");
+                return HttpNotFound();
+            }
+            return View(dON_THUOC);
         }
 
-        public ActionResult Delete()
+        // GET: Admin/QL_DonThuoc/Create
+        public ActionResult Create()
         {
-            DonThuoc.Clear();
-            Session["DonThuoc"] = DonThuoc;
-            return RedirectToAction("Index");
-        }
-        /// <summary>
-        /// ////////////////////////////////////////////////////////////////////////////////////
-        /// </summary>
-
-        public ActionResult DS_DonThuoc()
-        {
-            var model = db.DON_THUOC.ToList();
-            return View(model);
-        }
-
-        public ActionResult ThemDonThuoc()
-        {
+            ViewBag.Patient = GetAllPatient();
+            ViewBag.Medicine = GetAllMedicine();
+            ViewBag.Doctor = GetAllDoctor();
             ViewBag.ID_BACSI = new SelectList(db.BACSIs, "ID_BACSI", "TEN_BACSI");
             ViewBag.ID_SO_KHAM_BENH = new SelectList(db.SO_KHAM_BENH, "ID_SOKHAMBENH", "ID_SOKHAMBENH");
-            ViewBag.Cart = DonThuoc;
             return View();
         }
 
+        // POST: Admin/QL_DonThuoc/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult ThemDonThuoc(DON_THUOC model)
+        public ActionResult Create(PatientModel dON_THUOC)
         {
-            ValidateDonThuoc(model);
             if (ModelState.IsValid)
             {
-                using (var scope = new TransactionScope())
+                var benhNhan = db.BENH_NHAN.Find(dON_THUOC.HoTen);
+                if (benhNhan.SO_KHAM_BENH.Count == 0)
                 {
-                    model.NGAY_LAP = DateTime.Parse(DateTime.Now.ToString("MM/dd/yyyy HH:mm"));
-                    db.DON_THUOC.Add(model);
-                    db.SaveChanges();
-
-                    foreach (var item in DonThuoc)
-                    {
-                        db.DON_THUOC.Add(new DON_THUOC
-                        {
-                            ID_DON_THUOC = model.ID_DON_THUOC,
-                            ID_THUOC = item.THUOC.ID_THUOC
-                        });
-                    }
-                    db.SaveChanges();
-
-                    scope.Complete();
-                    Session["DonThuoc"] = null;
-                    return RedirectToAction("DS_DonThuoc");
+                    ModelState.AddModelError("Error", "Bệnh nhân chưa có sổ khám bệnh.");
+                    return View();
                 }
+                benhNhan.SO_KHAM_BENH.ElementAt(0).DON_THUOC.Add(new DON_THUOC
+                {
+                    CHUAN_DOAN = dON_THUOC.ChanDoan,
+                    CHI_DINH = dON_THUOC.ChiDinh,
+                    LOI_DAN = dON_THUOC.LoiDan,
+                    KET_QUA = dON_THUOC.KetQua,
+                    NGAY_LAP = dON_THUOC.NgayKhoiTao,
+                    ID_BACSI = dON_THUOC.BacSi,
+                    CHI_TIET_DON_THUOC = dON_THUOC.Thuocs.Select(item => new CHI_TIET_DON_THUOC
+                    {
+                        ID_THUOC = item.Id,
+                        SO_LUONG = item.Quantity
+                    }).ToList()
+                });
+                db.SaveChanges();
+                return RedirectToAction("Index", "QL_DonThuoc", new { area = "Admin" });
             }
 
-            ViewBag.Cart = DonThuoc;
-            ViewBag.ID_BACSI = new SelectList(db.BACSIs, "ID_BACSI", "TEN_BACSI", model.ID_BACSI);
-            ViewBag.ID_SO_KHAM_BENH = new SelectList(db.SO_KHAM_BENH, "ID_SOKHAMBENH", "ID_SOKHAMBENH", model.ID_SO_KHAM_BENH);
-
-            return View(model);
+            //ViewBag.ID_BACSI = new SelectList(db.BACSIs, "ID_BACSI", "TEN_BACSI", dON_THUOC.ID_BACSI);
+            //ViewBag.ID_SO_KHAM_BENH = new SelectList(db.SO_KHAM_BENH, "ID_SOKHAMBENH", "ID_SOKHAMBENH", dON_THUOC.ID_SO_KHAM_BENH);
+            return View(dON_THUOC);
         }
 
-        private void ValidateDonThuoc(DON_THUOC model)
+        // GET: Admin/QL_DonThuoc/Edit/5
+        public ActionResult Edit(int? id)
         {
-            if (DonThuoc.Count == 0)
-                ModelState.AddModelError("", "Chưa có thuốc trong đơn!");
+            ViewBag.Medicine = GetAllMedicine();
+            ViewBag.Doctor = GetAllDoctor();
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            DON_THUOC dON_THUOC = db.DON_THUOC.Find(id);
+            if (dON_THUOC == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ID_BACSI = new SelectList(db.BACSIs, "ID_BACSI", "TEN_BACSI", dON_THUOC.ID_BACSI);
+            ViewBag.ID_SO_KHAM_BENH = new SelectList(db.SO_KHAM_BENH, "ID_SOKHAMBENH", "ID_SOKHAMBENH", dON_THUOC.ID_SO_KHAM_BENH);
+            return View(dON_THUOC);
+        }
+
+        // POST: Admin/QL_DonThuoc/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        public ActionResult Edit(PatientModel dON_THUOC, int id)
+        {
+            var donthuoc = db.DON_THUOC.Find(id);
+            if (ModelState.IsValid)
+            {
+                donthuoc.CHUAN_DOAN = dON_THUOC.ChanDoan;
+                donthuoc.CHI_DINH = dON_THUOC.ChiDinh;
+                donthuoc.LOI_DAN = dON_THUOC.LoiDan;
+                donthuoc.KET_QUA = dON_THUOC.KetQua;
+                donthuoc.CHI_TIET_DON_THUOC = dON_THUOC.Thuocs.Select(item => new CHI_TIET_DON_THUOC
+                {
+                    ID_THUOC = item.Id,
+                    SO_LUONG = item.Quantity
+                }).ToList();
+                db.Entry(donthuoc).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Medicine = GetAllMedicine();
+            //ViewBag.ID_BACSI = new SelectList(db.BACSIs, "ID_BACSI", "TEN_BACSI", dON_THUOC.ID_BACSI);
+            //ViewBag.ID_SO_KHAM_BENH = new SelectList(db.SO_KHAM_BENH, "ID_SOKHAMBENH", "ID_SOKHAMBENH", dON_THUOC.ID_SO_KHAM_BENH);
+            return View(donthuoc);
+        }
+
+        //// GET: Admin/QL_DonThuoc/Delete/5
+        //public ActionResult Delete(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    DON_THUOC dON_THUOC = db.DON_THUOC.Find(id);
+        //    if (dON_THUOC == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(dON_THUOC);
+        //}
+
+        //// POST: Admin/QL_DonThuoc/Delete/5
+        //[HttpPost, ActionName("Delete")]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult DeleteConfirmed(int id)
+        //{
+        //    DON_THUOC dON_THUOC = db.DON_THUOC.Find(id);
+        //    db.DON_THUOC.Remove(dON_THUOC);
+        //    db.SaveChanges();
+        //    return RedirectToAction("Index");
+        //}
+
+        /// <summary>
+        /// Get All Medicine
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public List<THUOC> GetAllMedicine() => db.THUOCs.ToList();
+
+        /// <summary>
+        /// Get All Patient
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public List<BENH_NHAN> GetAllPatient() => db.BENH_NHAN.Where(item => item.SO_KHAM_BENH.Count > 0).ToList();
+
+        /// <summary>
+        /// Get All Doctor
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet]
+        public List<BACSI> GetAllDoctor() => db.BACSIs.ToList();
+
+
+        [HttpGet]
+        public JsonResult GetPatient(int id)
+        {
+            var benhNhan = db.BENH_NHAN.Find(id);
+            return Json(new BenhNhanModel
+            {
+                ID = benhNhan.ID_BENH_NHAN,
+                Ten = benhNhan.TEN_BN,
+                GioiTinh = benhNhan.GIOI_TINH,
+                Tuoi = DateTime.Now.Year - benhNhan.NGAY_SINH.Value.Year,
+                DiaChi = benhNhan.DIA_CHI
+            }, JsonRequestBehavior.AllowGet);
         }
 
         protected override void Dispose(bool disposing)
@@ -131,6 +201,51 @@ namespace VanLangDoctor.Areas.Admin.Controllers
                 db.Dispose();
             }
             base.Dispose(disposing);
+        }
+
+        public class BenhNhanModel
+        {
+            public int ID { get; set; }
+
+            public string Ten { get; set; }
+
+            public int Tuoi { get; set; }
+
+            public string GioiTinh { get; set; }
+
+            public string DiaChi { get; set; }
+        }
+
+        public class PatientModel
+        {
+            public int HoTen { get; set; }
+
+            public int Tuoi { get; set; }
+
+            public string GioiTinh { get; set; }
+
+            public string DiaChi { get; set; }
+
+            public DateTime NgayKhoiTao { get; set; }
+
+            public string ChanDoan { get; set; }
+
+            public string LoiDan { get; set; }
+
+            public string ChiDinh { get; set; }
+
+            public string KetQua { get; set; }
+
+            public int BacSi { get; set; }
+
+            public ThuocModel[] Thuocs { get; set; }
+        }
+
+        public class ThuocModel
+        {
+            public int Id { get; set; }
+
+            public int Quantity { get; set; }
         }
     }
 }
--- VanLangDoctor/VanLangDoctor/Areas/User/Controllers/CapnhatTKController.cs
+++ VanLangDoctor/VanLangDoctor/Areas/User/Controllers/CapnhatTKController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNet.Identity;
 using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web.Mvc;
 using VanLangDoctor.Models;
 
@@ -12,7 +14,7 @@ namespace VanLangDoctor.Areas.User.Controllers
         CP24Team02Entities db = new CP24Team02Entities();
 
         // GET: SinhVien/CapnhatTaiKhoan
-        public ActionResult CapnhatTK(string id_user)
+        public ActionResult CapnhatTK(string id_user, int? id_skb)
         {
             if (string.IsNullOrEmpty(id_user))
                 return RedirectToAction("CapnhatTK", new { id_user = User.Identity.GetUserId() });
@@ -27,6 +29,7 @@ namespace VanLangDoctor.Areas.User.Controllers
                 DIA_CHI = "",
                 AspNetUser = db.AspNetUsers.Find(id_user)
             };
+            ViewBag.Prescriptions = GetAllPrescriptions(id_skb);
             return View(user);
         }
 
@@ -61,6 +64,12 @@ namespace VanLangDoctor.Areas.User.Controllers
             db.SaveChanges();
             ViewBag.message = "Cập nhật thành công";
             return View("CapnhatTK", user);
+        }
+
+        [HttpGet]
+        public List<DON_THUOC> GetAllPrescriptions(int? id)
+        {
+            return db.DON_THUOC.OrderByDescending(t => t.NGAY_LAP).Where(t => t.SO_KHAM_BENH.BENH_NHAN.ID_BENH_NHAN == id).ToList();
         }
     }
 }
