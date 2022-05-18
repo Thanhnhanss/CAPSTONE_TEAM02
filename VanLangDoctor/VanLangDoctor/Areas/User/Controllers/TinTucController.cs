19fce86e7522bc3b6a85c7c54926c634180fa154
187PM06602 - DANG PHU HUNG - T5 <hungdp2612@gmail.com>
2022-05-18 15:20:26 +0700

    fix view create doctor, sokhambenh, ql dangkytvv, layout

--- VanLangDoctor/VanLangDoctor/Areas/User/Controllers/TinTucController.cs
+++ VanLangDoctor/VanLangDoctor/Areas/User/Controllers/TinTucController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PagedList;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
@@ -24,6 +25,57 @@ namespace VanLangDoctor.Areas.User.Controllers
             ViewBag.danhmuc = GetDanhMucTin();
             ViewBag.Top = GetTop3Views();
             return View(danhmuctin.OrderByDescending(t => t.NGAY_DANG).ToList());
+        }
+
+        public ViewResult Index(string sortOrder, string currentFilter, string searchString, int? page, int id_cate = 1)
+        {
+            ViewBag.Top1 = GetTop1View();
+            ViewBag.Top3 = GetTop3Views();
+            ViewBag.RecentArticles = GetRecentArticles();
+            var danhmuctin = db.TIN_TUC.Where(tk => tk.ID_Danhmuc_tin == id_cate).ToList();
+            ViewBag.danhmuc = GetDanhMucTin();
+            ViewBag.Top = GetTop3Views();
+            ViewBag.CurrentSort = sortOrder;
+            ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            ViewBag.DateSortParm = sortOrder == "Date" ? "date_desc" : "Date";
+
+            if (searchString != null)
+            {
+                page = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
+            ViewBag.CurrentFilter = searchString;
+
+            var students = from s in db.TIN_TUC
+                           select s;
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                students = students.Where(s => s.TEN_BAI_VIET.Contains(searchString)
+                                       || s.TAC_GIA.Contains(searchString));
+            }
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    students = students.OrderByDescending(s => s.TEN_BAI_VIET);
+                    break;
+                case "Date":
+                    students = students.OrderBy(s => s.NGAY_DANG);
+                    break;
+                case "date_desc":
+                    students = students.OrderByDescending(s => s.NGAY_DANG);
+                    break;
+                default:  // Name ascending 
+                    students = students.OrderBy(s => s.TEN_BAI_VIET);
+                    break;
+            }
+
+            int pageSize = 4;
+            int pageNumber = (page ?? 1);
+            return View(students.ToPagedList(pageNumber, pageSize));
         }
 
         private const string PICTURE_PATH = "~/Content/IMG_NEWS/";
