7103ed0ddf8597d8f3d58f9a7e2493d1f2d5c459
hotanminhtam <hotanminhtam1703@gmail.com>
2022-06-22 12:24:46 +0700

    commit

--- VanLangDoctor/VanLangDoctor/Areas/Admin/Controllers/QL_LichTuVanController.cs
+++ VanLangDoctor/VanLangDoctor/Areas/Admin/Controllers/QL_LichTuVanController.cs
@@ -1,30 +1,24 @@
-﻿using System;
+﻿using Google.Apis.Calendar.v3;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
+using System.Globalization;
 using System.Linq;
 using System.Net;
-using System.Web;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 using System.Web.Mvc;
 using VanLangDoctor.Models;
-
-using Google.Apis.Auth.OAuth2;
-using Google.Apis.Calendar.v3;
-using Google.Apis.Calendar.v3.Data;
-using Google.Apis.Services;
-using Google.Apis.Util.Store;
-using System.IO;
-using System.Threading;
-using Google.Apis.Auth.OAuth2.Flows;
-using Google.Apis.Auth.OAuth2.Mvc;
-using System.Threading.Tasks;
 
 namespace VanLangDoctor.Areas.Admin.Controllers
 {
     public class QL_LichTuVanController : Controller
     {
-        static string[] Scopes = { CalendarService.Scope.Calendar, CalendarService.Scope.CalendarEvents };
-        static string ApplicationName = "Google Calendar API.NET Quickstart";
+        //static string[] Scopes = { CalendarService.Scope.Calendar, CalendarService.Scope.CalendarEvents };
+        //static string ApplicationName = "Google Calendar API.NET Quickstart";
 
         private CP24Team02Entities db = new CP24Team02Entities();
 
@@ -57,65 +51,94 @@ namespace VanLangDoctor.Areas.Admin.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(DAT_LICH_TU_VAN dAT_LICH_TU_VAN)
+        public async Task<ActionResult> Edit(DAT_LICH_TU_VAN dAT_LICH_TU_VAN)
         {
             if (ModelState.IsValid)
             {
                 if (dAT_LICH_TU_VAN.TRANG_THAI == 1)
                 {
-                    try
-                    {
-                        UserCredential credential;
-                        var path = Server.MapPath("~/Content/") + "tamhtm.json";
-                        using (var stream = new FileStream(path, FileMode.Open, FileAccess.Read))
+                    var httpClient = HttpClientFactory.Create();
+                    var uri = new Uri("https://webexapis.com/v1/meetings");
+                    var bacSi = await db.BACSIs.FindAsync(dAT_LICH_TU_VAN.ID_BAC_SI);
+                    var start = dAT_LICH_TU_VAN.NGAY_KHAM.ToString("yyyy-MM-ddTHH\\:mm\\:sszzz", CultureInfo.InvariantCulture);
+                    var end = dAT_LICH_TU_VAN.NGAY_KHAM.AddHours(2).ToString("yyyy-MM-ddTHH\\:mm\\:sszzz", CultureInfo.InvariantCulture);
+                    var body = new Dictionary<string, string>
                         {
-                            /* The file token.json stores the user's access and refresh tokens, and is created
-                             automatically when the authorization flow completes for the first time. */
-                            string credPath = Server.MapPath("~/Content/") + "token.json";
-                            credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
-                                GoogleClientSecrets.FromStream(stream).Secrets,
-                                Scopes,
-                                "user",
-                                CancellationToken.None,
-                                new FileDataStore(credPath, true)).Result;
-                            Console.WriteLine("Credential file saved to: " + credPath);
-                        }
-
-                        //Create Google Calendar API service
-                        var service = new CalendarService(new BaseClientService.Initializer
-                        {
-                            HttpClientInitializer = credential,
-                            ApplicationName = ApplicationName
-                        });
-
-
-                        Event body = new Event();
-                        EventDateTime start = new EventDateTime();
-                        start.DateTime = dAT_LICH_TU_VAN.NGAY_KHAM;
-                        EventDateTime end = new EventDateTime();
-                        end.DateTime = dAT_LICH_TU_VAN.NGAY_KHAM + TimeSpan.FromHours(2);
-                        body.Start = start;
-                        body.End = end;
-                        body.Location = "Online meeting";
-                        body.Summary = "Tu van kham benh";
-                        body.ConferenceData = new ConferenceData();
-                        body.ConferenceData.CreateRequest = new CreateConferenceRequest();
-                        body.ConferenceData.CreateRequest.RequestId = Guid.NewGuid().ToString();
-                        body.ConferenceData.CreateRequest.ConferenceSolutionKey = new ConferenceSolutionKey
-                        {
-                            Type = "hangoutsMeet"
+                            { "title", $"Consulting with {bacSi.TEN_BACSI}" },
+                            // ISO 8601 datetime presentation
+                            { "start", start },
+                            { "end", end }
                         };
-                        EventsResource.InsertRequest request = new EventsResource.InsertRequest(service, body, "hotanminhtam1703@gmail.com");
-                        request.ConferenceDataVersion = 1;
-                        Event response = request.Execute();
-                        dAT_LICH_TU_VAN.LINK_GG = response.HangoutLink;
-
-
-                    }
-                    catch (FileNotFoundException e)
+                    var content = new FormUrlEncodedContent(body);
+                    var requestMessage = new HttpRequestMessage
                     {
-                        Console.WriteLine(e.Message);
+                        RequestUri = uri,
+                        Method = HttpMethod.Post,
+                        Content = content
+                    };
+                    requestMessage.Headers.Authorization = new AuthenticationHeaderValue("Bearer", Session["ACCESS_TOKEN"]?.ToString());
+                    var responseMessage = await httpClient.SendAsync(requestMessage);
+                    if (responseMessage.StatusCode == HttpStatusCode.Unauthorized ||
+                        responseMessage.StatusCode == HttpStatusCode.Forbidden)
+                    {
+                        return Redirect($"/Webex/Auth?redirectUrl=%2FAdmin%2FQL_LichTuVan%2FEdit%2F{dAT_LICH_TU_VAN.ID}");
                     }
+                    var responseMessageText = await responseMessage.Content.ReadAsStringAsync();
+                    var response = JsonConvert.DeserializeObject<WebexMeetingResposne>(responseMessageText);
+                    dAT_LICH_TU_VAN.LINK_GG = response.WebLink;
+                    //try
+                    //{
+                    //    UserCredential credential;
+                    //    var path = Server.MapPath("~/Content/") + "tamhtm.json";
+                    //    using (var stream = new FileStream(path, FileMode.Open, FileAccess.Read))
+                    //    {
+                    //        /* The file token.json stores the user's access and refresh tokens, and is created
+                    //         automatically when the authorization flow completes for the first time. */
+                    //        string credPath = Server.MapPath("~/Content/") + "token.json";
+                    //        credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    //            GoogleClientSecrets.FromStream(stream).Secrets,
+                    //            Scopes,
+                    //            "user",
+                    //            CancellationToken.None,
+                    //            new FileDataStore(credPath, true)).Result;
+                    //        Console.WriteLine("Credential file saved to: " + credPath);
+                    //    }
+
+                    //    //Create Google Calendar API service
+                    //    var service = new CalendarService(new BaseClientService.Initializer
+                    //    {
+                    //        HttpClientInitializer = credential,
+                    //        ApplicationName = ApplicationName
+                    //    });
+
+
+                    //    Event body = new Event();
+                    //    EventDateTime start = new EventDateTime();
+                    //    start.DateTime = dAT_LICH_TU_VAN.NGAY_KHAM;
+                    //    EventDateTime end = new EventDateTime();
+                    //    end.DateTime = dAT_LICH_TU_VAN.NGAY_KHAM + TimeSpan.FromHours(2);
+                    //    body.Start = start;
+                    //    body.End = end;
+                    //    body.Location = "Online meeting";
+                    //    body.Summary = "Tu van kham benh";
+                    //    body.ConferenceData = new ConferenceData();
+                    //    body.ConferenceData.CreateRequest = new CreateConferenceRequest();
+                    //    body.ConferenceData.CreateRequest.RequestId = Guid.NewGuid().ToString();
+                    //    body.ConferenceData.CreateRequest.ConferenceSolutionKey = new ConferenceSolutionKey
+                    //    {
+                    //        Type = "hangoutsMeet"
+                    //    };
+                    //    EventsResource.InsertRequest request = new EventsResource.InsertRequest(service, body, "hotanminhtam1703@gmail.com");
+                    //    request.ConferenceDataVersion = 1;
+                    //    Event response = request.Execute();
+                    //    dAT_LICH_TU_VAN.LINK_GG = response.HangoutLink;
+
+
+                    //}
+                    //catch (FileNotFoundException e)
+                    //{
+                    //    Console.WriteLine(e.Message);
+                    //}
                 }
 
                 db.Entry(dAT_LICH_TU_VAN).State = EntityState.Modified;
@@ -135,5 +158,16 @@ namespace VanLangDoctor.Areas.Admin.Controllers
             }
             base.Dispose(disposing);
         }
+    }
+
+    class WebexMeetingResposne
+    {
+        public string Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string SiteUrl { get; set; }
+
+        public string WebLink { get; set; }
     }
 }
---
+++ VanLangDoctor/VanLangDoctor/Controllers/WebexAuthController.cs
@@ -0,0 +1,64 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace VanLangDoctor.Controllers
+{
+    public class WebexAuthController : Controller
+    {
+        private const string CLIENT_ID = "C962ad74755f90c9b55962bc56d80d96a6922ed7636c733889013f70fab476784";
+
+        private const string CLIENT_SECRECT = "6f1bdf28c779fd26a57ae5e4ec0ad599aeb102bf3303df78fca3f8ca0bda9ef4";
+
+        private const string SCOPE = "meeting%3Aschedules_read%20meeting%3Aschedules_write";
+
+        private const string POST_REDIRECT_URI = "https://localhost:44311/webex/auth-callback";
+
+        [Route("webex/auth")]
+        public ActionResult Auth(string redirectUrl = "/")
+        {
+            redirectUrl = System.Net.WebUtility.UrlEncode(redirectUrl);
+            var encodedPostRedirectUrl = System.Net.WebUtility.UrlEncode(POST_REDIRECT_URI);
+            var url = $"https://webexapis.com/v1/authorize?response_type=code&client_id={CLIENT_ID}&redirect_uri={encodedPostRedirectUrl}&scope={SCOPE}&state={redirectUrl}";
+            return Redirect(url);
+        }
+
+        [Route("webex/auth-callback")]
+        public async Task<ActionResult> AuthCallbackAsync(string code, string state)
+        {
+            var httpClient = HttpClientFactory.Create();
+
+            var body = new Dictionary<string, string>
+            {
+                { "grant_type", "authorization_code" },
+                { "client_id", CLIENT_ID },
+                { "client_secret", CLIENT_SECRECT },
+                { "code", code },
+                { "redirect_uri", POST_REDIRECT_URI }
+            };
+            var content = new FormUrlEncodedContent(body);
+
+            var requestMessage = new HttpRequestMessage
+            {
+                Method = HttpMethod.Post,
+                RequestUri = new System.Uri("https://webexapis.com/v1/access_token"),
+                Content = content
+            };
+
+            var responseMessage = await httpClient.SendAsync(requestMessage);
+            responseMessage.EnsureSuccessStatusCode();
+            var message = await responseMessage.Content.ReadAsStringAsync();
+            var response = JsonConvert.DeserializeObject<WebexAccessTokenResponse>(message);
+            Session["ACCESS_TOKEN"] = response.AccessToken;
+            return Redirect(state);
+        }
+
+        class WebexAccessTokenResponse
+        {
+            [JsonProperty("access_token")]
+            public string AccessToken { get; set; }
+        }
+    }
+}
